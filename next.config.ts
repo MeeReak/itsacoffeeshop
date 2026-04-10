86151488e219081199b1d2bc7e1d0834fa555c35
MeeReak <meereak168@gmail.com>
2026-04-10 17:03:12 +0700

    fix deploy failded

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['next-typegen'],
   images: {
     dangerouslyAllowLocalIP: true,
     remotePatterns: [
